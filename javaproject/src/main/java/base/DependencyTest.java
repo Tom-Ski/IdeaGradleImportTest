e8b7ce4db8cc1239166d67dad7169d10c493e056
tomski <tomwojciechowski@asidik.com>
2018-04-09 19:20:52 +0100

    Update to latest android tools, Gradle version and IDEA EAP 2018

---
+++ javaproject/src/main/java/base/DependencyTest.java
@@ -0,0 +1,16 @@
+import com.google.common.collect.ImmutableBiMap;
+
+public class DependencyTest {
+
+    public static void main (String[] args) {
+        ImmutableBiMap.Builder<Object, Object> builder = ImmutableBiMap.builder();
+
+        builder.put("Key 1", "Value 1");
+        builder.put("Key 2", "Value 2");
+        builder.put("Key 3", "Value 3");
+
+        ImmutableBiMap<Object, Object> build = builder.build();
+
+        build.entrySet().forEach(System.out::println);
+    }
+}
